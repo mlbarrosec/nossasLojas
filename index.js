5c874c0b4ee186539e229cf20b5b8476d70dd5fa
mlbarrosec <mlbarrosec@gmail.com>
2019-04-08 17:17:04 -0300

    put server online

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('teste');
+
+//Rodando servidor com express
+var express = require('express');
+var app = express();
+
+app.listen(3000,function(){
+    console.log("Servidor rodando na porta 3000");
+});
