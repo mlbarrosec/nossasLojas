d2fb6a9eac1b7d778eb58b6d870e7075004de63f
mlbarrosec <mlbarrosec@gmail.com>
2019-04-09 09:09:51 -0300

    config consign in file custon-express.js

--- config/custon-express.js
+++ config/custon-express.js
@@ -1,9 +1,10 @@
 //Arquivo de configuração do express
 var express = require ('express'); //importa a biblióteca express
+var consign = require('consign'); //import consign api
 
 module.exports = function() { //esse modulo exporta a variavel app com express e suas funcionalidades
     
     var app = express();
-
+    consign().into(app); 
     return app;
 }
